8a8e2f6a2a296dde678e9b5d66274b63d0b4ff94
marcelodate-create <marcelo45957341827@gmail.com>
2026-03-05 06:18:05 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{padding:"40px",fontFamily:"Arial"}}>
-      <h1>FadReview</h1>
-      <p>Sistema de entrega profissional de serviços.</p>
-      <p>Projeto em desenvolvimento.</p>
-    </main>
-  )
-}
